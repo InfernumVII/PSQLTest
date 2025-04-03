31deb6b39982b2e902e0b4778d1c160d85aac8e4
InfenrumVII <egor554shved@gmail.com>
2025-04-03 03:43:30 +0300

    3333

--- src/App.java
+++ src/App.java
@@ -1,19 +1,30 @@
+import java.io.Console;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Locale;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class App {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/postgres";
-        String user = "user";
-        String password = "2281337";
-        
+        String user = "username";
+        String pass = "pass";
+        Properties info = new Properties();
+        info.setProperty("user",user);
+        info.setProperty("password",pass);
+        //URL к базе состоит из протокола:подпротокола://[хоста]:[порта_СУБД]/[БД] и других_сведений
+        //Создаём соединение
         try {
-            Connection conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected to PostgreSQL successfully!");
-            conn.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+            Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", info);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
+        
     }
 }
